5d7e7ef10312b48d766646ee7557704e4ab8a331
saul-manich <saulmamani73@gmail.com>
2016-08-30 23:26:13 -0400

    LoginPHP

---
+++ login/Login.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css" >
+		
+		body{
+			background-image: url(fon.png);
+			background-size: cover;
+			font-family: verdana;
+
+
+		}
+		.aa{
+		width: 400px;
+		height: 260px;
+		background-color: rgba(0, 0, 0, 0.3);
+		margin:0 auto;
+		margin-top: 200px ;
+		padding-top: 10px;
+		padding-left: 20px;
+		border-radius: 15px;
+		-webkit-border-radius:15px;
+		-o-border-radius:15px;
+		-moz-border-radius:15px;
+		color: white;
+		font-weight: bolder;
+		box-shadow: inset -4px -4px rgba(0, 0, 0, 0.3);
+
+	}
+		.aa input[type="text"]{
+			width: 240px;
+			height: 35px;
+			border:0;
+			-webkit-border-radius:5px;
+			-o-border-radius:5px;
+			-moz-border-radius:5px;
+			padding-left: 5px;
+		}
+
+		.aa input[type="password"]{
+			width: 240px;
+			height: 35px;
+			border:0;
+			border-radius: 5px;
+			-webkit-border-radius:5px;
+			-o-border-radius:5px;
+			-moz-border-radius:5px;
+			padding-left: 5px;
+		}
+
+		.aa input[type="submit"]{
+			width: 100px;
+			height: 35px;
+			border:0;
+			margin:left;
+			border-radius: 5px;
+			-webkit-border-radius:5px;
+			-o-border-radius:5px;
+			-moz-border-radius:5px;
+			background-color: skyblue;
+			font-weight: bolder;
+		}
+	</style>
+</head>
+<body>
+<div class="aa" align="center">
+<h2>Login</h2>
+<form>
+	<input type="text" placeholder="Ingrese su Usuario"><br><br>
+	<input type="password" placeholder="Ingrese su Password"><br><br>
+	<input type="submit" value="Entrar" >
+	
+	
+</form>
+</div>
+</body>
+</html>
